cb37b1830f78f60fe732d3ee687020e56cbca7d8
akudrya <alexandra.kudryashova@skolkovotech.ru>
2013-07-21 17:12:16 +0000

    edit web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   	var resp = fs.readFileSync('index.html');
-	response.send(resp);
+	response.writeContinue(resp);
 });
 
 var port = process.env.PORT || 5000;
